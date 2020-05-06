0c8c54ff88987089634011f3a89d199e8100ec62
Connor <connor.thorson@gmail.com>
2020-05-05 22:14:25 -0700

    added horizontal anchor links, added more text

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ t1.to(".menu", 1, {
 
 t1.set(".hamburger", {className: "+=hamburger active"})
 
-t1.staggerFrom(".menu a", 0.2, {x: -200, opacity: 0, ease:Expo.easeOut}, 0.1);
+t1.staggerFrom(".menu a", 0.1, {opacity: 0, ease:Expo.easeOut}, 0.1);
 
 t1.reverse();
 $('.hamburger').on("click", function() {
@@ -46,4 +46,45 @@ $('.hamburger').on("click", function() {
 $('.menu a').on("click", function() {
      t1.reversed(!t1.reversed());
 });
+
+
+
+var contentSections = $('.cd-section'),
+    navigationItems = $('#cd-vertical-nav a');
+
+updateNavigation();
+$(window).on('scroll', function(){
+    updateNavigation();
+});
+
+//smooth scroll to the section
+navigationItems.on('click', function(event){
+    event.preventDefault();
+    smoothScroll($(this.hash));
+});
+//smooth scroll to second section
+$('.cd-scroll-down').on('click', function(event){
+    event.preventDefault();
+    smoothScroll($(this.hash));
+});
+
+function updateNavigation() {
+    contentSections.each(function(){
+        var $this = $(this);
+        var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+        if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+            navigationItems.eq(activeSection).addClass('is-selected');
+            console.log('selected')
+        }else {
+            navigationItems.eq(activeSection).removeClass('is-selected');
+        }
+    });
+}
+
+function smoothScroll(target) {
+    $('body,html').animate(
+        {'scrollTop':target.offset().top},
+        600
+    );
+}
 
